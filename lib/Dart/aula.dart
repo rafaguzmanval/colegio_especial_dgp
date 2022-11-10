7b944925dec33ce11c4afaa9aa2be7ac8b392637
Blanca Abril Gonzalez <32962533+blancabgz@users.noreply.github.com>
2022-11-10 11:06:57 +0100

    Cambio de nombre y documentación añadida

---
+++ lib/Dart/aula.dart
@@ -0,0 +1,18 @@
+
+/*
+*   Archivo: aula.dart
+*
+*   Descripción:
+*   Se implementará en un futuro. Contiene los métodos para realizar un filtrado de usuarios para los listados de profesores y alumnos. 
+
+* */
+
+class Aula{
+  var idClase = "";
+  var nombreClase = "";
+  var profesores = [];
+  var alumnos = [];
+
+  Aula(this.nombreClase,this.profesores,this.alumnos);
+
+}
