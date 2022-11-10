ad5a83041c3920560615a460b40cdd9dea4502e3
Blanca Abril Gonzalez <32962533+blancabgz@users.noreply.github.com>
2022-11-10 11:19:48 +0100

    Includes que no utilizados eliminados + documentacion

--- lib/Dart/main.dart
+++ lib/Dart/main.dart
@@ -1,13 +1,25 @@
-import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/cupertino.dart';
+
+/*
+*   Archivo: main.dart
+*
+*   Descripción: 
+*   Inicia flutter con los colores de la paleta de colores.
+*   
+*   Includes:
+*   material.dart : Se utiliza para dar colores y diseño a la aplicacion.
+*   firebase_core.dart : Se utiliza para acceder a los servicios del servidor. 
+*   flutter_local_notifications.dart : Se utiliza para inicializar notificaciones.
+*   firebase_options.dart : Se utiliza para acceder con nuestro token a nuestro servidor. 
+*   notificacion.dart : Se utiliza para crear notificaciones y que aparezcan en el dispositivo.
+*   flutter_localizations.dart : Se utiliza para traducir widgets al español.
+* */
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'firebase_options.dart';
-import '../Flutter/myhomepage.dart';
 import '../Flutter/loginpage.dart';
 import 'notificacion.dart';
-import 'package:localization/localization.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 var user = "";
