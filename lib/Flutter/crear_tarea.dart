85e25503fa21b10adf14a46aa7520ebd11f41e32
jpanequeg <jpanequeg@gmail.com>
2022-11-10 11:45:22 +0100

    Tareas

--- lib/Flutter/crear_tarea.dart
+++ lib/Flutter/crear_tarea.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:colegio_especial_dgp/Dart/sesion.dart';
 import 'package:colegio_especial_dgp/Dart/rol.dart';
 
-
 import 'package:colegio_especial_dgp/Dart/acceso_bd.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -20,22 +19,14 @@ import '../Dart/main.dart';
 import '../Dart/notificacion.dart';
 import '../Dart/tarea.dart';
 
-enum SeleccionImagen{
-  camara,
-  galeria,
-  video
-}
+enum SeleccionImagen { camara, galeria, video }
 
-
-class CrearTarea extends StatefulWidget{
-
+class CrearTarea extends StatefulWidget {
   @override
   CrearTareaState createState() => CrearTareaState();
-
 }
 
-class CrearTareaState extends State<CrearTarea>{
-
+class CrearTareaState extends State<CrearTarea> {
   var db = FirebaseFirestore.instance;
 
   AccesoBD base = new AccesoBD();
@@ -48,270 +39,230 @@ class CrearTareaState extends State<CrearTarea>{
   var creando = false;
   var mensajeDeValidacion = "";
 
-
-
   final controladorNombre = TextEditingController();
   final controladorTexto = TextEditingController();
 
-
-
   ///Cuándo se pasa de página es necesario que todos los controladores de los formularios y de los reproductores de vídeo se destruyan.
   @override
-  void dispose(){
-
+  void dispose() {
     super.dispose();
     controladorVideo.dispose();
     controladorNombre.dispose();
     controladorTexto.dispose();
   }
 
-
-
   @override
-  void initState(){
+  void initState() {
     super.initState();
     //Notificacion.showBigTextNotification(title: "Vaya vaya", body: "¿Que dices? ¿creando tu primera tarea?", fln: flutterLocalNotificationsPlugin);
 
     Sesion.paginaActual = this;
-
   }
 
   ///introduce en el atributo @fototomada la imagen, @seleccion nos indica si el método va a ser desde la cámara o de galería
-  seleccionarImagen(seleccion) async{
-
+  seleccionarImagen(seleccion) async {
     try {
-      if(seleccion == SeleccionImagen.camara)
-        {
-      print("Se va a abrir la cámara de fotos");
-        fotoTomada =  await capturador.pickImage(
-            source: ImageSource.camera,
-            imageQuality: 15,
-
-        );}
-    else if(seleccion == SeleccionImagen.galeria)
-      {
-        print("Se va a coger una foto de la galería");
-        fotoTomada =  await capturador.pickImage(
-            source: ImageSource.gallery,
-            imageQuality: 15);
-      }
-    else {
-        print("Hacer un video");
-        await capturador.pickVideo(
+      if (seleccion == SeleccionImagen.camara) {
+        print("Se va a abrir la cámara de fotos");
+        fotoTomada = await capturador.pickImage(
           source: ImageSource.camera,
-        ).then((value) async{
+          imageQuality: 15,
+        );
+      } else if (seleccion == SeleccionImagen.galeria) {
+        print("Se va a coger una foto de la galería");
+        fotoTomada = await capturador.pickImage(
+            source: ImageSource.gallery, imageQuality: 15);
+      } else {
+        print("Hacer un video");
+        await capturador
+            .pickVideo(
+          source: ImageSource.camera,
+        )
+            .then((value) async {
           videoTomado = value;
-          controladorVideo = await VideoPlayerController.file(File(value?.path as String));
+          controladorVideo =
+              await VideoPlayerController.file(File(value?.path as String));
           await controladorVideo.initialize();
           actualizar();
 
           print(controladorVideo.value.duration.toString());
         });
-
-
-
-
       }
-
-    }
-    catch(e){
+    } catch (e) {
       print(e);
     }
     actualizar();
   }
 
-
   /// Este es el build de la clase MyHomePage que devuelve toda la vista génerica más la vista especial de cada usuario.
   @override
-  Widget build(BuildContext context){
-    
-    return
-
-      new Scaffold(
-
-        appBar:AppBar(
-
-          title: Text('Crea una nueva tarea'),
-        ),
-        body: SingleChildScrollView(
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Crea una nueva tarea'),
+      ),
+      body: SingleChildScrollView(
           padding: EdgeInsets.symmetric(vertical: 0, horizontal: 0),
-        child: Column(
-        children: [
-        if (Sesion.rol == Rol.alumno.toString()) ...[
-        VistaAlumno(),
-        ] else if (Sesion.rol == Rol.profesor.toString()) ...[
-        VistaProfesor()
-        ] else if (Sesion.rol == Rol.administrador.toString()) ...[
-        VistaAdministrador()
-        ]
-        ],
-        )),
-
-      );
-
-
+          child: Column(
+            children: [
+              if (Sesion.rol == Rol.alumno.toString()) ...[
+                VistaAlumno(),
+              ] else if (Sesion.rol == Rol.profesor.toString()) ...[
+                VistaProfesor()
+              ] else if (Sesion.rol == Rol.administrador.toString()) ...[
+                VistaAdministrador()
+              ]
+            ],
+          )),
+    );
   }
 
   ///Este método devuelve toda la vista que va a ver el profesor en un Widget.
-  Widget VistaProfesor()
-  {
+  Widget VistaProfesor() {
     Navigator.pop(context);
-    return
-      Container(
-      );
+    return Container();
   }
 
   ///Este método devuelve toda la vista que va a ver el alumno en un Widget.
-  Widget VistaAlumno()
-  {
+  Widget VistaAlumno() {
     Navigator.pop(context);
-    return
-      Container(
-      );
+    return Container();
   }
 
-
   ///Este método devuelve toda la vista que va a ver el administrador en un Widget.
-  Widget VistaAdministrador()
-  {
-    return
-      Container(
-        //padding: EdgeInsets.symmetric(vertical: 0,horizontal: 200),
-        alignment: Alignment.center,
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 5,
-            ),
-            SizedBox(
-              width: 500,
-              child: TextField(
-                obscureText: false,
-                maxLength: 40,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Introduce el nombre de la tarea *',
-                ),
-                controller: controladorNombre,
+  Widget VistaAdministrador() {
+    return Container(
+      //padding: EdgeInsets.symmetric(vertical: 0,horizontal: 200),
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: 5,
+          ),
+          SizedBox(
+            width: 500,
+            child: TextField(
+              obscureText: false,
+              maxLength: 40,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Introduce el nombre de la tarea *',
               ),
+              controller: controladorNombre,
             ),
-            SizedBox(
-              height: 5,
-            ),
-            SizedBox(
-              width: 500,
-              child: TextField(
-                obscureText: false,
-                maxLength: 60,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Introduce una Descripción *',
-                ),
-                controller: controladorTexto,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          SizedBox(
+            width: 500,
+            child: TextField(
+              obscureText: false,
+              maxLength: 60,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Introduce una Descripción *',
               ),
+              controller: controladorTexto,
             ),
-
-            const Text(
-              "Elige una foto para la tarea: *",
-              style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            ElevatedButton(
-                child: Text('Haz una foto'),
-                onPressed: (){seleccionarImagen(SeleccionImagen.camara);}
-            ),
-            const Text(
-              "Elige un pictograma para la tarea: *",
-              style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            ElevatedButton(
-                child: Text('Elige un pictograma de tu galería'),
-                onPressed: (){seleccionarImagen(SeleccionImagen.galeria);}
-            ),
-            const Text(
-              "Elige un videotutorial para la tarea: *",
-              style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            ElevatedButton(
-                child: Text('Haz un videotutorial'),
-                onPressed: (){seleccionarImagen(SeleccionImagen.video);}
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              decoration: BoxDecoration(border: Border.all(width: 2)),
-              height: 120,
-              width: 200,
-                child: fotoTomada == null
-                  ? Center(child: Text('Ninguna foto tomada ****'))
-                  : Center(child: Image.file(File(fotoTomada.path))),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              decoration: BoxDecoration(border: Border.all(width: 2)),
-              height: 200,
-              width: 200,
-              child: videoTomado == null
-                  ? Center(child: Text('Ningun video tomado ****'))
-                  : Center(child:ReproductorVideo(controladorVideo)),
-            ),
-
-
-            Text(mensajeDeValidacion),
-
-
-            Visibility(
-                visible: !creando,
-                child:
-
-            ElevatedButton(
-              child: Text("Crear nueva tarea",
-                style: TextStyle(
-                    color: Colors.white,
-                ),
-              ),
+          ),
+          const Text(
+            "Elige una foto para la tarea: *",
+            style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          ElevatedButton(
+              child: Text('Haz una foto'),
               onPressed: () {
-                crearTarea();
-              },
-
-            )
-            ),
-
-            SizedBox(
-              height: 10,
-            ),
-
-
-            Visibility(
-                visible: creando,
-                child: new CircularProgressIndicator()
-
-            ),
-
-          ],
-        ),
-      );
+                seleccionarImagen(SeleccionImagen.camara);
+              }),
+          const Text(
+            "Elige un pictograma para la tarea: *",
+            style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          ElevatedButton(
+              child: Text('Elige un pictograma de tu galería'),
+              onPressed: () {
+                seleccionarImagen(SeleccionImagen.galeria);
+              }),
+          const Text(
+            "Elige un videotutorial para la tarea: *",
+            style: TextStyle(fontSize: 20.0, height: 2.0, color: Colors.black),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          ElevatedButton(
+              child: Text('Haz un videotutorial'),
+              onPressed: () {
+                seleccionarImagen(SeleccionImagen.video);
+              }),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all(width: 2)),
+            height: 120,
+            width: 190,
+            child: fotoTomada == null
+                ? Center(child: Text('Ninguna foto tomada ****'))
+                : Center(child: Image.file(File(fotoTomada.path))),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all(width: 2)),
+            margin: EdgeInsets.only(right: 70, left: 70, top: 20, bottom: 20),
+            child: videoTomado == null
+                ? Container(
+                    child: Text('Ningun video tomado ****'),
+                    padding: EdgeInsets.only(
+                        top: 100, bottom: 100, right: 10, left: 10),
+                  )
+                : Container(
+                    child: ReproductorVideo(controladorVideo),
+                    padding: EdgeInsets.only(
+                        top: 10, bottom: 10, right: 10, left: 10),
+                  ),
+          ),
+          Text(mensajeDeValidacion),
+          Visibility(
+              visible: !creando,
+              child: Container(
+                  margin: EdgeInsets.only(top: 0),
+                  child: ElevatedButton(
+                    child: Text(
+                      "Crear nueva tarea",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      crearTarea();
+                    },
+                  ))),
+          SizedBox(
+            height: 10,
+          ),
+          Visibility(visible: creando, child: new CircularProgressIndicator()),
+        ],
+      ),
+    );
   }
 
   //Método para registrar usuario
-  crearTarea() async
-  {
-
-    if(controladorNombre.text.isNotEmpty && videoTomado != null && fotoTomada != null)
-    {
+  crearTarea() async {
+    if (controladorNombre.text.isNotEmpty &&
+        videoTomado != null &&
+        fotoTomada != null) {
       creando = true;
       actualizar();
 
@@ -326,13 +277,10 @@ class CrearTareaState extends State<CrearTarea>{
       var videos = [];
       videos.add(File(videoTomado.path));
 
-      var orden = ["T","I","V"];
-
+      var orden = ["T", "I", "V"];
 
       Tarea tarea = Tarea();
-      tarea.setTarea(
-          nombre,textos,imagenes,videos,orden);
-
+      tarea.setTarea(nombre, textos, imagenes, videos, orden);
 
       await base.crearTarea(tarea).then((value) {
         creando = false;
@@ -344,62 +292,47 @@ class CrearTareaState extends State<CrearTarea>{
           videoTomado = null;
 
           mensajeDeValidacion =
-          "Tarea creada correctamente\nPuedes volver a crear otra tarea:";
-        }
-        else {
+              "Tarea creada correctamente\nPuedes volver a crear otra tarea:";
+        } else {
           mensajeDeValidacion = "Fallo al crear tarea, inténtelo de nuevo";
         }
 
         actualizar();
       });
+    } else {
+      mensajeDeValidacion = "Es necesario rellenar todos los campos";
+      actualizar();
     }
-    else
-      {
-        mensajeDeValidacion = "Es necesario rellenar todos los campos";
-        actualizar();
-      }
   }
 
-
-
-  Widget ReproductorVideo(controlador)
-  {
-    return
-      Container(
+  Widget ReproductorVideo(controlador) {
+    return Container(
         //padding: EdgeInsets.symmetric(vertical: 0,horizontal: 200),
-          child:Column(
-              children:[
+        child: Column(children: [
+      ElevatedButton(
+        onPressed: () {
+          if (controlador.value.isPlaying) {
+            controlador.pause();
+          } else {
+            controlador.play();
+          }
+          setState(() {});
+        },
+        child: AspectRatio(
+            aspectRatio: controlador.value.aspectRatio,
+            child: VideoPlayer(controlador)),
+      ),
+      Icon(
+        controlador.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        size: 20,
+        semanticLabel: controlador.value.isPlaying ? "Pausa" : "Reanudar",
+      ),
+      Container(
+        //duration of video
+        child: Text("Total Duration: " + controlador.value.duration.toString()),
+      ),
 
-                ElevatedButton(
-
-                  onPressed: (){
-                              if(controlador.value.isPlaying){
-                              controlador.pause();
-                              }else{
-                              controlador.play();
-                              }
-                              setState(() {
-                              });
-                      },
-                    child: AspectRatio(
-                        aspectRatio: controlador.value.aspectRatio ,
-                        child: VideoPlayer(controlador)
-                    ),
-                ),
-
-                Icon(
-                    controlador.value.isPlaying?Icons.pause:Icons.play_arrow,
-                  size: 20,
-                  semanticLabel: controlador.value.isPlaying?"Pausa":"Reanudar",
-                ),
-
-
-
-                Container( //duration of video
-                  child: Text("Total Duration: " + controlador.value.duration.toString()),
-                ),
-
-                /*
+      /*
                 Container(
                     child: VideoProgressIndicator(
                         controlador,
@@ -411,25 +344,10 @@ class CrearTareaState extends State<CrearTarea>{
                         )
                     )
                 ),*/
-
-              ]
-          )
-
-      );
+    ]));
   }
 
-
-  void actualizar() async
-  {
-    setState((){});
+  void actualizar() async {
+    setState(() {});
   }
-
 }
-
-
-
-
-
-
-
-
