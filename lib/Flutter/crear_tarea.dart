8bcb5f0fa8639f959081487061daa61481737f13
rafaguzmanval <rafagval@gmail.com>
2022-11-11 19:03:17 +0100

    busqueda de pictogramas finalizada

--- lib/Flutter/crear_tarea.dart
+++ lib/Flutter/crear_tarea.dart
@@ -66,11 +66,17 @@ class CrearTareaState extends State<CrearTarea> {
   @override
   void dispose() {
     super.dispose();
+    if(controladorVideo != null)
     controladorVideo.dispose();
+
+    if(controladorNombre != null)
     controladorNombre.dispose();
+
+    if(controladorTexto != null)
     controladorTexto.dispose();
-    controladorStream.close();
-    controladorBusqueda.dispose();
+
+    if(controladorBusqueda != null)
+      controladorBusqueda.dispose();
 
   }
 
@@ -187,8 +193,24 @@ class CrearTareaState extends State<CrearTarea> {
                       ),),
                     if(snapshot.data.toString() != "")...[
 
-                      for (int i = 0; i < longitud && i < 20; i++)
-                        buscarARASAAC(msg, i)
+                      for (int i = 0; i < longitud && i < 20; i=i+2)
+
+                        Container(child:
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            for(int j = i; j < i+2 && j < longitud; j++)
+                          if(!msg[i]["sex"] && !msg[i]["violence"])...[
+                            Flexible(flex: 50,
+                                child:
+                            buscarARASAAC(msg, j))
+
+                          ]
+                        ],)
+                       )
+
+
                       ]
                   ]);
 
@@ -203,9 +225,8 @@ class CrearTareaState extends State<CrearTarea> {
 }
 
 buscarARASAAC(mensaje,i){
+
   return Container(
-      width: 100,
-      height: 100,
       child: 
           ElevatedButton(
             style: ButtonStyle(
@@ -217,11 +238,19 @@ buscarARASAAC(mensaje,i){
                 actualizar();
                 Navigator.pop(context, false);
               },
-              child: Image.network(
-                  "https://api.arasaac.org/api/pictograms/" +
-                      mensaje[i]["_id"].toString())),
+              child:
+                  Column(
+                    children:[
+                      Center(child:Text(mensaje[i]["keywords"][0]["keyword"],style: TextStyle(color: Colors.black),)),
+                      Image.network(
+                          "https://api.arasaac.org/api/pictograms/" +
+                              mensaje[i]["_id"].toString()),
+                    ]
+                  )
+
               
-          );
+          )
+  );
 
 }
 
