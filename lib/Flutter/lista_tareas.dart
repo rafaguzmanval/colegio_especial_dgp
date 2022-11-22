f0359a219b4dfb2b1dae9436bb4df5b21f936bd3
jpanequeg <jpanequeg@gmail.com>
2022-11-22 15:05:05 +0100

    Casi Edicion Tareas

--- lib/Flutter/lista_tareas.dart
+++ lib/Flutter/lista_tareas.dart
@@ -12,13 +12,13 @@
 *   perfil_profesor.dart : Para acceder al perfil del profeosr
 * */
 
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:colegio_especial_dgp/Dart/sesion.dart';
 import 'package:colegio_especial_dgp/Dart/guardado_local.dart';
 import 'package:colegio_especial_dgp/Flutter/perfil_profesor.dart';
 import 'package:colegio_especial_dgp/Dart/rol.dart';
 import 'package:colegio_especial_dgp/Dart/acceso_bd.dart';
+import 'package:colegio_especial_dgp/Flutter/perfil_tarea.dart';
 import 'package:flutter/material.dart';
 
 class ListaTareas extends StatefulWidget {
@@ -31,7 +31,6 @@ class ListaTareasState extends State<ListaTareas> {
 
   double offSetActual = 0;
   ScrollController homeController = new ScrollController();
-
 
   @override
   void initState() {
@@ -51,57 +50,67 @@ class ListaTareasState extends State<ListaTareas> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-            appBar: AppBar(
-              leading: IconButton(
-                  icon: Icon(Icons.arrow_back_ios_new, color: GuardadoLocal.colores[2]),
-                  onPressed: (){Navigator.pop(context);}),
-              title: Text('Lista de Profesores',style: TextStyle(color: GuardadoLocal.colores[2]),),
-            ),
-            body: Stack(children: [
-              OrientationBuilder(
-                builder: (context, orientation) =>
+        appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios_new,
+                  color: GuardadoLocal.colores[2]),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          title: Text(
+            'Lista de Profesores',
+            style: TextStyle(color: GuardadoLocal.colores[2]),
+          ),
+        ),
+        body: Stack(children: [
+          OrientationBuilder(
+            builder: (context, orientation) =>
                 orientation == Orientation.portrait
                     ? buildPortrait()
                     : buildLandscape(),
-              ),
-              Container(
-                alignment: FractionalOffset(0.98, 0.01),
-                child: FloatingActionButton(
-                    heroTag: "botonUp",
-                    child: Icon(Icons.arrow_upward,color: GuardadoLocal.colores[2],),
-                    elevation: 1.0,
-                    onPressed: () {
-                      offSetActual -= 100.0;
-                      if (offSetActual < homeController.position.minScrollExtent)
-                        offSetActual = homeController.position.minScrollExtent;
+          ),
+          Container(
+            alignment: FractionalOffset(0.98, 0.01),
+            child: FloatingActionButton(
+                heroTag: "botonUp",
+                child: Icon(
+                  Icons.arrow_upward,
+                  color: GuardadoLocal.colores[2],
+                ),
+                elevation: 1.0,
+                onPressed: () {
+                  offSetActual -= 100.0;
+                  if (offSetActual < homeController.position.minScrollExtent)
+                    offSetActual = homeController.position.minScrollExtent;
 
-                      homeController.animateTo(
-                        offSetActual, // change 0.0 {double offset} to corresponding widget position
-                        duration: Duration(seconds: 1),
-                        curve: Curves.easeOut,
-                      );
-                    }),
-              ),
-              Container(
-                alignment: FractionalOffset(0.98, 0.99),
-                child: FloatingActionButton(
-                    heroTag: "botonDown",
-                    child: Icon(Icons.arrow_downward, color: GuardadoLocal.colores[2]),
-                    elevation: 1.0,
-                    onPressed: () {
-                      offSetActual += 100;
+                  homeController.animateTo(
+                    offSetActual, // change 0.0 {double offset} to corresponding widget position
+                    duration: Duration(seconds: 1),
+                    curve: Curves.easeOut,
+                  );
+                }),
+          ),
+          Container(
+            alignment: FractionalOffset(0.98, 0.99),
+            child: FloatingActionButton(
+                heroTag: "botonDown",
+                child:
+                    Icon(Icons.arrow_downward, color: GuardadoLocal.colores[2]),
+                elevation: 1.0,
+                onPressed: () {
+                  offSetActual += 100;
 
-                      if (offSetActual > homeController.position.maxScrollExtent)
-                        offSetActual = homeController.position.maxScrollExtent;
+                  if (offSetActual > homeController.position.maxScrollExtent)
+                    offSetActual = homeController.position.maxScrollExtent;
 
-                      homeController.animateTo(
-                        offSetActual, // change 0.0 {double offset} to corresponding widget position
-                        duration: Duration(seconds: 1),
-                        curve: Curves.easeOut,
-                      );
-                    }),
-              ),
-            ]));
+                  homeController.animateTo(
+                    offSetActual, // change 0.0 {double offset} to corresponding widget position
+                    duration: Duration(seconds: 1),
+                    curve: Curves.easeOut,
+                  );
+                }),
+          ),
+        ]));
   }
 
   ///Este método devuelve toda la vista que va a ver el profesor en un Widget.
@@ -127,35 +136,37 @@ class ListaTareasState extends State<ListaTareas> {
       //padding: EdgeInsets.symmetric(vertical: 0,horizontal: 200),
       child: Column(
         children: [
-          for (int i = 0; i < tareas.length; i++)
+          for (int i = 0; i < Sesion.tareas.length; i++)
             Container(
-                width: 100,
-                height: 100,
+                width: 200,
+                height: 220,
                 margin: EdgeInsets.all(10),
                 alignment: Alignment.center,
                 child: ElevatedButton(
                   child: Column(
                     children: [
                       Text(
-                        tareas[i].nombre.toString().toUpperCase(),
+                        Sesion.tareas[i].nombre.toString().toUpperCase(),
+                        textAlign: TextAlign.center,
                         style: TextStyle(
+                          fontSize: 25,
                           color: GuardadoLocal.colores[2],
                         ),
                       ),
-                      Image.network(
-                        tareas[i].foto,
-                        width: 100,
-                        height: 70,
-                        fit: BoxFit.fill,
-                      ),
+                      if (!Sesion.tareas[i].imagenes.isEmpty) ...[
+                        Image.network(
+                          Sesion.tareas[i].imagenes[0],
+                          width: 150,
+                          height: 150,
+                          fit: BoxFit.fill,
+                        ),
+                      ]
                     ],
                   ),
                   onPressed: () {
-                    Sesion.seleccion = tareas[i];
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => PerfilProfesor()));
+                    Sesion.seleccion = Sesion.tareas[i];
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => PerfilTarea()));
                   },
                 ))
         ],
@@ -205,7 +216,7 @@ class ListaTareasState extends State<ListaTareas> {
 
   // metodo para cargar la lista de profesores
   cargarProfesores() async {
-    tareas = await Sesion.db.consultarTodosProfesores();
+    Sesion.tareas = await Sesion.db.consultarTodasLasTareas();
     actualizar();
   }
 
