076ac368e64d27373f9c1fe1df1f225452b5dd3b
jpanequeg <jpanequeg@gmail.com>
2022-12-06 12:55:22 +0100

    PASOS

--- lib/Flutter/ver_pasos.dart
+++ lib/Flutter/ver_pasos.dart
@@ -20,27 +20,59 @@ class VerPasosState extends State<VerPasos> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("PASOS",style: TextStyle(fontSize:25, color: GuardadoLocal.colores[2],fontWeight: FontWeight.w800), textAlign: TextAlign.center,)),
+      appBar: AppBar(
+          title: Center(
+              child: Text(
+        "PASOS",
+        style: TextStyle(
+            fontSize: 25,
+            color: GuardadoLocal.colores[2],
+            fontWeight: FontWeight.w800),
+        textAlign: TextAlign.center,
+      ))),
       body: SingleChildScrollView(
           child: Column(
         children: [
-          for (int i = 0; i < tarea.textos.length; i++)...[
-            SizedBox(height: 10,),
+          for (int i = 0; i < tarea.textos.length; i++) ...[
+            SizedBox(
+              height: 10,
+            ),
             Container(
                 child: Column(children: [
               Container(
                   decoration: BoxDecoration(border: Border.all(width: 1)),
                   padding: EdgeInsets.all(5),
-                  child:Text("PASO " + (i + 1).toString().toUpperCase(),style: TextStyle(fontSize:25, color: GuardadoLocal.colores[0],fontWeight: FontWeight.w800))),
+                  child: Text("PASO " + (i + 1).toString().toUpperCase()+": ",
+                      style: TextStyle(
+                          fontSize: 40,
+                          color: GuardadoLocal.colores[0],
+                          fontWeight: FontWeight.w800))),
+                  SizedBox(
+                    height: 20,
+                  ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text(tarea.textos[i].toUpperCase(), style: TextStyle(fontWeight: FontWeight.bold,fontSize:25, color: GuardadoLocal.colores[0])),
-                  Image.network(tarea.imagenes[i]),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.only(left: 10),
+                        child: Text(tarea.textos[i].toUpperCase(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                                color: GuardadoLocal.colores[0])),
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Image.network(tarea.imagenes[i]))),
                 ],
               )
             ]))
-        ]],
+          ]
+        ],
       )),
     );
   }
