37627ec5f18517f2f0dd1edaba573d4b3712a447
rafaguzmanval <rafagval@gmail.com>
2022-12-08 17:52:08 +0100

    clase mensaje

---
+++ lib/Dart/mensaje.dart
@@ -0,0 +1,10 @@
+
+class Mensaje{
+  String idUsuarioEmisor;
+  String idUsuarioReceptor;
+  String tipo;
+  String contenido;
+  String fechaEnvio;
+  Mensaje(this.idUsuarioEmisor,this.idUsuarioReceptor,this.tipo,this.contenido,this.fechaEnvio);
+
+}
